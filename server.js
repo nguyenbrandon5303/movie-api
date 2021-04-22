d865190ba20bcb63bc4edb2a05944d8e162b1e40
Brandon Nguyen <51946258+nguyenbrandon5303@users.noreply.github.com>
2021-04-21 18:11:24 -0700

    First commit

---
+++ server.js
@@ -0,0 +1,33 @@
+const http = require('http'),
+      url = require('url'),
+      fs = require('fs');
+
+http.createServer((request, response) => {
+  let addr = request.url,
+      q = url.parse(addr, true),
+      filePath = '';
+
+  if (q.pathname.includes('documentation')) {
+    filePath = (__dirname + '/documentation.html');
+  } else {
+    filePath = 'index.html';
+  }
+
+  fs.appendFile('log.txt', 'URL: ' + addr + '\nTimestamp: ' + new Date() + '\n\n', (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log('Added to log.');
+    }
+  });
+
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      throw err;
+    }
+      response.writeHead(200, {'Content-Type': 'text/html'});
+      response.write(data);
+      response.end('new server');
+  });
+
+}).listen(8080);
