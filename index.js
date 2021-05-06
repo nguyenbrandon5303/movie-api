c4c3343899c7e90aefd3205c97099c6ae365002b
Brandon Nguyen <51946258+nguyenbrandon5303@users.noreply.github.com>
2021-05-06 15:00:39 -0700

    updated mongoose connection

--- index.js
+++ index.js
@@ -12,7 +12,8 @@ const Movies = Models.Movie,
 
 const {check, validationResult } = require('express-validator');
 
-mongoose.connect('mongodb://localhost:27017/myFlixDB', { useNewUrlParser: true, useUnifiedTopology: true });
+// mongoose.connect('mongodb://localhost:27017/myFlixDB', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const app  = express();
 
